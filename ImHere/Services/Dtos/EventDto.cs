c001496940b13755a6cdf9d7bee805411cd7abc0
Brady Marston <bradymarston@live.com>
2019-11-18 21:23:35 -0600

    Made EventDto.Name required

--- ImHere/Services/Dtos/EventDto.cs
+++ ImHere/Services/Dtos/EventDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace ImHere.Services.Dtos
     public class EventDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public EventScheduleInfoDtoBase Schedule { get; set; } = new OneTimeScheduleInfoDto();
     }
 }
