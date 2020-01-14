8566781f6dbd79efc32c96c16246ff72cdc539c9
Brady Marston <bradymarston@live.com>
2020-01-14 12:05:54 -0600

    Added a ReportController to output CSVs

---
+++ ImHere/Controllers/ReportController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImHere.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImHere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly StudentService _studentService;
+
+        public ReportController(StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet("student-list")]
+        public async Task<IActionResult> GetStudentList ()
+        {
+            var students = await _studentService.GetStudentsAsync();
+
+            var fileContent = "First Name,Last Name,Phone Number,Email\n";
+
+            foreach (var student in students)
+                fileContent += $"{student.FirstName},{student.LastName},{student.Phone},{student.Email}\n";
+
+            return File(Encoding.UTF8.GetBytes(fileContent), "text/csv", "studentlist.csv");
+        }
+    }
+}
