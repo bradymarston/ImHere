e37a2d2b543b6ccff3b8cfb2d9ad6ed80e981596
Brady Marston <bradymarston@live.com>
2020-02-23 20:19:27 -0600

    Added prospective student option

---
+++ ImHere/Data/Migrations/20200218024601_AddProspectiveStudentType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImHere.Data.Migrations
+{
+    public partial class AddProspectiveStudentType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("StudentTypes", "Description", "Prospective Student");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("StudentTypes", "Description", "Prospective Student");
+        }
+    }
+}
