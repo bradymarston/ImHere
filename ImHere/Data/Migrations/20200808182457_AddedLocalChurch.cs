eb5894b9c2809230e19b317d206aa16f312657d1
Brady Marston <bradymarston@live.com>
2020-08-08 13:26:09 -0500

    Added LocalChurch enum field to Student

---
+++ ImHere/Data/Migrations/20200808182457_AddedLocalChurch.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImHere.Data.Migrations
+{
+    public partial class AddedLocalChurch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocalChurch",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocalChurch",
+                table: "Students");
+        }
+    }
+}
--- ImHere/Data/Models/Student.cs
+++ ImHere/Data/Models/Student.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace ImHere.Data.Models
 {
+    public enum LocalChurch
+    {
+        None,
+        Methodist,
+        NonMethodist
+    }
     public class Student
     {
         public int Id { get; set; }
         public int StudentTypeId { get; set; }
         public StudentType StudentType { get; set; }
         public bool IsMethodist { get; set; }
+        public LocalChurch LocalChurch { get; set; }
         [Required]
         public string FirstName { get; set; }
         [Required]
